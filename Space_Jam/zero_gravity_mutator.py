4450b1225bf1d42bff96a1c78faccdfea9b8d438
ViliamVadocz <viliam.vadocz@gmail.com>
2019-08-18 22:41:24 +0200

    Some tweaks suggested by Kipje

--- Space_Jam/zero_gravity_mutator.py
+++ Space_Jam/zero_gravity_mutator.py
@@ -1,10 +1,11 @@
 '''Run this file to enable microgravity. Have fun!'''
 
+import time
+
 from rlbot.utils.structures.game_interface import GameInterface
 from rlbot.utils.structures.game_data_struct import GameTickPacket
 from rlbot.utils.logging_utils import get_logger
 from rlbot.utils.game_state_util import GameState, BallState, CarState, Physics, Vector3
-from rlbot.utils import rate_limiter
 
 class Observer():
     def __init__(self):
@@ -16,53 +17,53 @@ class Observer():
     def main(self):
         # Create packet
         packet = GameTickPacket()
-
-        rate_limit = rate_limiter.RateLimiter(60)
+        last_game_time = 0.0
 
         while True:
             # Update packet
             self.game_interface.update_live_data_packet(packet)
-
-            # Parameter:
-            KICKOFF_BALL_HEIGHT = 700
-
-            # Finds delta time per tick.
             game_time = packet.game_info.seconds_elapsed
-            try:
-                dt = game_time - last_game_time
-            except:
-                dt = 1 / 60
-            last_game_time = game_time
 
-            # Calculates the acceleration due to gravity per tick.
-            gravity = packet.game_info.world_gravity_z * dt
+            if last_game_time == game_time:
+                time.sleep(0.001)
 
-            # Cancels out gravity if the round is active.
-            if packet.game_info.is_round_active:
-                # Cancels out each car.
-                car_states = {}
-                for i in range(packet.num_cars):
-                    car = packet.game_cars[i].physics
-                    if car.location.z > 20:
-                        car_states.update({i : CarState(physics=Physics(velocity=Vector3(z = car.velocity.z - gravity)))})
+            else:
+                # Parameter:
+                KICKOFF_BALL_HEIGHT = 700
 
-                # Cancels out the ball.
-                ball = packet.game_ball.physics
+                # Finds delta time per tick.
+                try:
+                    dt = game_time - last_game_time
+                except:
+                    dt = 1 / 120
+                last_game_time = game_time
 
-                if packet.game_info.is_kickoff_pause and round(ball.location.z) != KICKOFF_BALL_HEIGHT:
-                    # Places the ball in the air on kickoff.
-                    ball_state = BallState(Physics(location = Vector3(z=KICKOFF_BALL_HEIGHT), velocity=Vector3(z=0)))
-                else:
-                    ball_state = BallState(Physics(velocity=Vector3(z = ball.velocity.z - gravity)))
+                # Calculates the acceleration due to gravity per tick.
+                gravity = packet.game_info.world_gravity_z * dt
 
-                # Uses state setting to set the game state.
-                game_state = GameState(ball=ball_state, cars=car_states)
-                # It doesn't hurt and apparently helps.
-                self.game_interface.set_game_state(game_state)
+                # Cancels out gravity if the round is active.
+                if packet.game_info.is_round_active:
+                    # Cancels out each car.
+                    car_states = {}
+                    for i in range(packet.num_cars):
+                        car = packet.game_cars[i].physics
+                        if car.location.z > 20:
+                            car_states.update(
+                                {i: CarState(physics=Physics(velocity=Vector3(z=car.velocity.z - gravity)))})
 
-                rate_limit.acquire()
+                    # Cancels out the ball.
+                    ball = packet.game_ball.physics
 
+                    if packet.game_info.is_kickoff_pause and round(ball.location.z) != KICKOFF_BALL_HEIGHT:
+                        # Places the ball in the air on kickoff.
+                        ball_state = BallState(Physics(location=Vector3(z=KICKOFF_BALL_HEIGHT), velocity=Vector3(z=0)))
+                    else:
+                        ball_state = BallState(
+                            Physics(velocity=Vector3(z=ball.velocity.z - gravity)))
+
+                    # Uses state setting to set the game state.
+                    game_state = GameState(ball=ball_state, cars=car_states)
+                    self.game_interface.set_game_state(game_state)
 
 if __name__ == "__main__":
-    obv = Observer()
-    
+    obv = Observer()    # nice.
