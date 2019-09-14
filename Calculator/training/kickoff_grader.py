bdce6120d853cb95eaac5dd39fb442fb6f9332e6
ViliamVadocz <viliam.vadocz@gmail.com>
2019-09-14 19:24:21 +0200

    added a wait timer to make sure small pads reset

--- Calculator/training/kickoff_grader.py
+++ Calculator/training/kickoff_grader.py
@@ -12,9 +12,9 @@ from rlbottraining.grading.training_tick_packet import TrainingTickPacket
 @dataclass
 class QuickResetKickoffGrader(CompoundGrader):
 
-    def __init__(self, timeout_seconds = 5):
+    def __init__(self, timeout_seconds = 5.5, reset_time = 5):
         super().__init__([
-            PassOnBallMoveFromKickoff(),
+            PassOnBallMoveFromKickoff(reset_time),
             FailOnTimeout(timeout_seconds)
         ])
 
@@ -22,15 +22,27 @@ class QuickResetKickoffGrader(CompoundGrader):
 @dataclass
 class PassOnBallMoveFromKickoff(Grader):
 
+    def __init__(self, reset_time):
+        self.reset_time = reset_time
+        self.initial_seconds_elapsed = None
+
+
     class PassDueToMovedBall(Pass):
         def __repr__(self):
             return f'{super().__repr__()}: Ball has moved from the start position.'
 
     def on_tick(self, tick: TrainingTickPacket) -> Optional[Grade]:
 
-        ball = tick.game_tick_packet.game_ball.physics.location
+        packet = tick.game_tick_packet
+        game_time = packet.game_info.seconds_elapsed
+        ball = packet.game_ball.physics.location
 
-        if  -100 > ball.x or ball.x > 100 or -100 > ball.y or ball.y > 100:
+        if self.initial_seconds_elapsed is None:
+            self.initial_seconds_elapsed = game_time
+
+        ball_out_of_centre = -100 > ball.x or ball.x > 100 or -100 > ball.y or ball.y > 100
+        reset_time_reached = game_time >= self.initial_seconds_elapsed + self.reset_time
+        if ball_out_of_centre and reset_time_reached:
             return self.PassDueToMovedBall()
         else:
             return None
