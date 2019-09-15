47289dd9b7a8cb54394f60027b40a02d1ed64d38
ViliamVadocz <viliam.vadocz@gmail.com>
2019-09-15 18:33:41 +0200

    Small changes based on DomNomNom suggestions

--- Calculator/training/kickoff_exercise.py
+++ Calculator/training/kickoff_exercise.py
@@ -1,51 +1,52 @@
 from dataclasses import dataclass, field
-from typing import Union
-from pathlib import Path
 from math import pi
+from pathlib import Path
+from typing import List, Tuple
+
+from rlbot.matchconfig.match_config import MatchConfig, PlayerConfig, Team
+from rlbot.utils.game_state_util import BallState, CarState, GameState, Physics, Rotator, Vector3
 
 from kickoff_grader import QuickResetKickoffGrader
-
-from rlbot.utils.game_state_util import GameState, BallState, CarState, Physics, Vector3, Rotator
-from rlbot.matchconfig.match_config import Team, PlayerConfig
-
-from rlbottraining.training_exercise import TrainingExercise, Playlist
 from rlbottraining.grading.grader import Grader
+from rlbottraining.match_configs import make_empty_match_config
+from rlbottraining.paths import BotConfigs
 from rlbottraining.rng import SeededRandomNumberGenerator
+from rlbottraining.training_exercise import TrainingExercise, Playlist
 
 
 @dataclass
-class KickoffType:
+class SpawnLocation:
     pos: Vector3
     rot: Rotator
 
 
-# Default kickoffs.
-right_corner = KickoffType(Vector3(-2048, -2560, 18), Rotator(0, 0.25*pi, 0))
-left_corner = KickoffType(Vector3(2048, -2560, 18), Rotator(0, 0.75*pi, 0))
-back_right = KickoffType(Vector3(-256, -3840, 18), Rotator(0, 0.5*pi, 0))
-back_left = KickoffType(Vector3(256.0, -3840, 18), Rotator(0, 0.5*pi, 0))
-straight = KickoffType(Vector3(0, -4608, 18), Rotator(0, 0.5*pi, 0))
+class Spawns():
+    """Default Kickoffs Spawns (BLUE)"""
+    CORNER_R = SpawnLocation(Vector3(-2048, -2560, 18), Rotator(0, 0.25*pi, 0))
+    CORNER_L = SpawnLocation(Vector3(2048, -2560, 18), Rotator(0, 0.75*pi, 0))
+    BACK_R = SpawnLocation(Vector3(-256, -3840, 18), Rotator(0, 0.5*pi, 0))
+    BACK_L = SpawnLocation(Vector3(256.0, -3840, 18), Rotator(0, 0.5*pi, 0))
+    STRAIGHT = SpawnLocation(Vector3(0, -4608, 18), Rotator(0, 0.5*pi, 0))
 
 
 @dataclass
 class KickoffExercise(TrainingExercise):
     grader: Grader = field(default_factory=QuickResetKickoffGrader)
-    kickoffs: Union[tuple, list] = ()
+    spawns: List[SpawnLocation] = field(default_factory=list)
 
     def make_game_state(self, rng: SeededRandomNumberGenerator) -> GameState:
 
         num_players = self.match_config.num_players
-        assert num_players == len(
-            self.kickoffs), 'Number of players does not match the number of kickoffs given.'
+        assert num_players == len(self.spawns), 'Number of players does not match the number of spawns.'
 
         car_states = {}
         for index in range(num_players):
             car_states[index] = CarState(
                 boost_amount=33,
                 physics=Physics(
-                    location=self.kickoffs[index].pos,
+                    location=self.spawns[index].pos,
                     velocity=Vector3(0, 0, 0),
-                    rotation=self.kickoffs[index].rot,
+                    rotation=self.spawns[index].rot,
                     angular_velocity=Vector3(0, 0, 0)
                 )
             )
@@ -65,23 +66,22 @@ class KickoffExercise(TrainingExercise):
 
 def make_default_playlist() -> Playlist:
 
-    # Choose which kickoffs you want to test.
-    # The length of kickoffs should match the number of players in the match_config.
+    # Choose which spawns you want to test.
+    # The length of spawns should match the number of players in the match_config.
 
     exercises = [
-        #KickoffExercise(name='Both Corners', kickoffs=(right_corner,left_corner)),
-        KickoffExercise(name='Right Corner', kickoffs=(right_corner,)),
-        KickoffExercise(name='Left Corner', kickoffs=(left_corner,)),
-        KickoffExercise(name='Back Right', kickoffs=(back_right,)),
-        KickoffExercise(name='Back Left', kickoffs=(back_left,)),
-        KickoffExercise(name='Straight', kickoffs=(straight,)),
+        #KickoffExercise('Both Corners', spawns=[Spawns.CORNER_R, Spawns.CORNER_L]),
+        KickoffExercise('Right Corner', spawns=[Spawns.CORNER_R]),
+        KickoffExercise('Left Corner', spawns=[Spawns.CORNER_L]),
+        KickoffExercise('Back Right', spawns=[Spawns.BACK_R]),
+        KickoffExercise('Back Left', spawns=[Spawns.BACK_L]),
+        KickoffExercise('Straight', spawns=[Spawns.STRAIGHT]),
     ]
 
     for ex in exercises:
         ex.match_config.player_configs = [
             # Replace with path to your bot.
-            PlayerConfig.bot_config(Path(__file__).absolute(
-            ).parent.parent / 'Calculator.cfg', Team.BLUE)
-        ] * len(ex.kickoffs)
+            PlayerConfig.bot_config(BotConfigs.simple_bot, Team.BLUE) for _ in ex.spawns
+        ]
 
     return exercises
--- Calculator/training/kickoff_grader.py
+++ Calculator/training/kickoff_grader.py
@@ -1,10 +1,11 @@
 from dataclasses import dataclass
-from typing import Optional
+from math import sqrt
+from typing import Optional, Union
 
 from rlbot.training.training import Pass, Grade
 
-from rlbottraining.grading.grader import Grader
 from rlbottraining.common_graders.compound_grader import CompoundGrader
+from rlbottraining.grading.grader import Grader
 
 from rlbottraining.common_graders.timeout import FailOnTimeout
 from rlbottraining.grading.training_tick_packet import TrainingTickPacket
@@ -12,21 +13,19 @@ from rlbottraining.grading.training_tick_packet import TrainingTickPacket
 @dataclass
 class QuickResetKickoffGrader(CompoundGrader):
 
-    def __init__(self, timeout_seconds = 5.5, reset_time = 5):
+    def __init__(self, timeout_seconds = 4.5, min_exercise_duration = 4, min_ball_displacement = 100):
         super().__init__([
-            PassOnBallMoveFromKickoff(reset_time),
+            PassOnBallMoveFromKickoff(min_exercise_duration, min_ball_displacement),
             FailOnTimeout(timeout_seconds)
         ])
 
 
 @dataclass
 class PassOnBallMoveFromKickoff(Grader):
-
-    def __init__(self, reset_time):
-        self.reset_time = reset_time
-        self.initial_seconds_elapsed = None
-
-
+    min_exercise_duration : Union[int, float]
+    min_ball_displacement : Union[int, float]
+    initial_seconds_elapsed : type(None) = None
+    ball_moved_flag : bool = False
     class PassDueToMovedBall(Pass):
         def __repr__(self):
             return f'{super().__repr__()}: Ball has moved from the start position.'
@@ -40,9 +39,13 @@ class PassOnBallMoveFromKickoff(Grader):
         if self.initial_seconds_elapsed is None:
             self.initial_seconds_elapsed = game_time
 
-        ball_out_of_centre = -100 > ball.x or ball.x > 100 or -100 > ball.y or ball.y > 100
-        reset_time_reached = game_time >= self.initial_seconds_elapsed + self.reset_time
-        if ball_out_of_centre and reset_time_reached:
+        ball_moved = sqrt(ball.x**2 + ball.y**2 + (ball.z-93)**2) >= self.min_ball_displacement
+        min_exercise_duration_reached = game_time >= self.initial_seconds_elapsed + self.min_exercise_duration
+
+        if ball_moved and not self.ball_moved_flag:
+            self.ball_moved_flag = True
+
+        if self.ball_moved_flag and min_exercise_duration_reached:
             return self.PassDueToMovedBall()
         else:
             return None
