6d4b8992f93a78f421d47ffd13712c2a4fdf25c6
ViliamVadocz <viliam.vadocz@gmail.com>
2019-09-15 11:22:47 +0200

    Multiplied bot config by number of kickoffs

--- Calculator/training/kickoff_exercise.py
+++ Calculator/training/kickoff_exercise.py
@@ -13,12 +13,11 @@ from rlbottraining.grading.grader import Grader
 from rlbottraining.rng import SeededRandomNumberGenerator
 
 
-
-
 @dataclass
 class KickoffType:
     pos: Vector3
     rot: Rotator
+
 
 # Default kickoffs.
 right_corner = KickoffType(Vector3(-2048, -2560, 18), Rotator(0, 0.25*pi, 0))
@@ -30,13 +29,14 @@ straight = KickoffType(Vector3(0, -4608, 18), Rotator(0, 0.5*pi, 0))
 
 @dataclass
 class KickoffExercise(TrainingExercise):
-    grader : Grader = field(default_factory=QuickResetKickoffGrader)
-    kickoffs : Union[tuple,list] = ()
+    grader: Grader = field(default_factory=QuickResetKickoffGrader)
+    kickoffs: Union[tuple, list] = ()
 
     def make_game_state(self, rng: SeededRandomNumberGenerator) -> GameState:
 
         num_players = self.match_config.num_players
-        assert num_players == len(self.kickoffs), 'Number of players does not match the number of kickoffs given.'
+        assert num_players == len(
+            self.kickoffs), 'Number of players does not match the number of kickoffs given.'
 
         car_states = {}
         for index in range(num_players):
@@ -65,22 +65,23 @@ class KickoffExercise(TrainingExercise):
 
 def make_default_playlist() -> Playlist:
 
-    # Choose which kickoffs you want to test. 
+    # Choose which kickoffs you want to test.
     # The length of kickoffs should match the number of players in the match_config.
 
     exercises = [
-        KickoffExercise(name='Both Corners', kickoffs=(right_corner,left_corner)),
-        #KickoffExercise(name='Right Corner', kickoffs=(right_corner,))
-        #KickoffExercise(name='Left Corner', kickoffs=(left_corner,)),
-        #KickoffExercise(name='Back Right', kickoffs=(back_right,)),
-        #KickoffExercise(name='Back Left', kickoffs=(back_left,)),
-        #KickoffExercise(name='Straight', kickoffs=(straight,)),
+        #KickoffExercise(name='Both Corners', kickoffs=(right_corner,left_corner)),
+        KickoffExercise(name='Right Corner', kickoffs=(right_corner,)),
+        KickoffExercise(name='Left Corner', kickoffs=(left_corner,)),
+        KickoffExercise(name='Back Right', kickoffs=(back_right,)),
+        KickoffExercise(name='Back Left', kickoffs=(back_left,)),
+        KickoffExercise(name='Straight', kickoffs=(straight,)),
     ]
 
     for ex in exercises:
         ex.match_config.player_configs = [
-            PlayerConfig.bot_config(Path(__file__).absolute().parent.parent / 'Calculator.cfg', Team.BLUE),
-            PlayerConfig.bot_config(Path(__file__).absolute().parent.parent / 'Calculator.cfg', Team.BLUE),
-        ]
+            # Replace with path to your bot.
+            PlayerConfig.bot_config(Path(__file__).absolute(
+            ).parent.parent / 'Calculator.cfg', Team.BLUE)
+        ] * len(ex.kickoffs)
 
     return exercises
